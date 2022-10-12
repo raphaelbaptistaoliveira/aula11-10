6841c1ec6f3d65322c3414fc494527686b5d0a7e
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-11 21:50:50 -0300

    Aula 11-10

---
+++ ATIVIDADE 11-10.java
@@ -0,0 +1,13 @@
+public class ATIVIDADE 11-10 {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 10;
+
+        System.out.println(x < y);
+        System.out.println(x == y);
+        System.out.println(x >= y);
+        System.out.println(x > y);
+        System.out.println(x != y);
+        System.out.println(x <= y);
+    }
+}
