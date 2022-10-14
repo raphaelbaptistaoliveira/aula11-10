583dfe574af9019239fa84098b70a2c80c61ad39
raphael baptista <raphaelbaptista3@gmail.com>
2022-10-14 19:43:53 -0300

    organizando pasta

--- ExemploOperadorTernario.java
+++
@@ -1,7 +0,0 @@
-public class ExemploOperadorTernario {
-    public static void main(String[] args) {
-        int hora = 20;
-        String turno = (hora >= 18) ? "Noite" : "Dia";
-        System.out.println(turno);
-    }
-}
